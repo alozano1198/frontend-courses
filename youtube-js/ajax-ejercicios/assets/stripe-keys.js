e37950a34c37aed9baa283de775ad1f40e99209f
glozanop <glozanop@miumg.edu.gt>
2024-06-29 15:03:10 -0600

    update js

--- youtube-js/ajax-ejercicios/assets/stripe-keys.js
+++
@@ -1,6 +0,0 @@
-export default {
-  public:
-    "pk_test_51NYcEBLUSBkPeRcbDemPRFV2tv3C0LQGLGNqJoOdTI0LJXbS6Y6ofSd2nvsGcgHG4AWyuCWP1iNbHMu8T2N5qzAu00jpbAG3Cl",
-  secret:
-    "sk_test_51NYcEBLUSBkPeRcbmgw2xF0Hq9LiNtrqGEwpaT164ETs3MvZiju0zd2lXf7h0x1EuqfEeDF23wA6lS9oXSwThGGj00WKqFRkBJ",
-};
